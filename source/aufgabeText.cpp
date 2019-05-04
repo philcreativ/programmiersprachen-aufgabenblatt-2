ed40e9029f429fb32390f95df677d1c1912fc54a
philcreativ <philcodes01@gmail.com>
2019-05-04 15:03:45 +0200

    Aufgabe 2.2 b

--- source/aufgabeText.cpp
+++ source/aufgabeText.cpp
@@ -1,8 +1,25 @@
 //
-##ifndef AUFGABETEXT
+#ifndef AUFGABETEXT
 #define AUFGABETEXT
 
 
+#endif //AUFGABETEXT
 
- value
-#endif
+
+/*
+
+Aufgabe 2.2_____________________________
+
+The include Gaurds
+these helps to ensure that we do not redefine (declare)
+and already defined class or variable that we have already
+defined earlier. That prevents duplication and inconsistences.
+
+If not used, this results in the Redefinition error of the 
+specific class / previously defined here error
+_________________________________________
+
+
+
+
+*/
